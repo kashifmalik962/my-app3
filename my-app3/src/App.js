da940c907aad5f1b50fe0bc18b7052918d91e6b5
Kashif Malik <kashifmalik962@gmail.com>
2024-03-04 11:40:12 +0530

    Initial commit

---
+++ my-app3/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Navbar from './MyComponents/Navbar';
+import Main from './MyComponents/Main';
+import About from './MyComponents/About';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Remove Switch
+
+
+function App() {
+  
+  return (
+    <>
+    <Router>
+      <div className="app-container">
+        <Navbar title='HHC'/>
+        <Routes>
+          <Route path='/' element={<Main heading='Enter text for analyze'/>} />
+          <Route path='/About' element={<About/>} />
+        </Routes>
+        </div>
+    </Router>
+    </>
+  )
+}
+
+export default App;
